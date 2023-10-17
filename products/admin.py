dba9704cc96dfdc9e666317c3421fbc6e02fbf72
siobhain <sobrien@live.ie>
2023-10-17 03:20:04 +0000

    Products - Add Category, Collection & Product models & Register with Admin

--- products/admin.py
+++ products/admin.py
@@ -1,3 +1,40 @@
 from django.contrib import admin
+from .models import Category, Collection, Product
 
 # Register your models here.
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = (
+        'friendly_name',
+        'name',
+        'engravable',
+        'sizeable',
+    )
+
+class CollectionAdmin(admin.ModelAdmin):
+    list_display = (
+        'friendly_name',
+        'name',
+        'description',
+    )
+
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = (
+        'name',
+        'description',
+        'category',
+        'collection',
+        'sku',
+        'price',
+        'rating',
+        'engrave_text',
+        'engrave_style',
+        'engrave_cost',
+        'image_url',
+        'image'
+    )
+    ordering = ('name',)
+
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Collection, CollectionAdmin)
+admin.site.register(Product, ProductAdmin)
--- products/models.py
+++ products/models.py
@@ -3,8 +3,13 @@ from django.db import models
 # Create your models here.
 
 class Category(models.Model):
-    name = models.CharField(max_length=254)
-    friendly_name = models.CharField(max_length=254, null=True, blank=True)
+    name = models.CharField(max_length=20)
+    friendly_name = models.CharField(max_length=25, null=True, blank=True)
+    engravable = models.BooleanField(default=False)
+    sizeable = models.BooleanField(default=False)
+
+    class Meta:
+        verbose_name_plural = 'Categories'
 
     def __str__(self):
         return self.name
@@ -12,9 +17,11 @@ class Category(models.Model):
     def get_friendly_name(self):
         return self.friendly_name
 
+
 class Collection(models.Model):
-    name = models.CharField(max_length=254)
-    friendly_name = models.CharField(max_length=254, null=True, blank=True)
+    name = models.CharField(max_length=20)
+    friendly_name = models.CharField(max_length=25, null=True, blank=True)
+    description = models.CharField(default="Complete the Description ASAP", max_length=254)
 
     def __str__(self):
         return self.name
@@ -24,15 +31,62 @@ class Collection(models.Model):
 
 
 class Product(models.Model):
+    name = models.CharField(max_length=50)
+    description = models.TextField()
+    # categories = models.ManyToManyField(Category)
     category = models.ForeignKey('Category', null=True, blank=True, on_delete=models.SET_NULL)
     collection = models.ForeignKey('Collection', null=True, blank=True, on_delete=models.SET_NULL)
     sku = models.CharField(max_length=254, null=True, blank=True)
-    name = models.CharField(max_length=254)
-    description = models.TextField()
     price = models.DecimalField(max_digits=6, decimal_places=2)
     rating = models.DecimalField(max_digits=6, decimal_places=2, null=True, blank=True)
+    engrave_text = models.CharField(max_length=12, null=True, blank=True)
+    engrave_style = models.CharField(max_length=12, null=True, blank=True)
+    engrave_cost = models.DecimalField(max_digits=6, decimal_places=2, null=True, blank=True)
+    # Want to remove image_url as think not necessary if AWS enabled
     image_url = models.URLField(max_length=1024, null=True, blank=True)
     image = models.ImageField(null=True, blank=True)
+    
+    SIZE_CHOICES = []
+    RING_SIZES = [
+        ('D', 'D'),
+        ('E', 'E'),
+        ('F', 'F'),
+        # Add more once get this working
+    ]
+
+    CHAIN_LENGTH = [
+        ('Small', 'Small 16 inch'),
+        ('Medium', 'Medium 18 inch'),
+        ('Large', 'Large 20 inch'),
+    ]
+
+    size = models.CharField(
+        max_length=10,
+        choices=SIZE_CHOICES,
+        blank=True,
+        null=True,
+    )
+
+
+# Want to  Dynamically set size choices based on category
+# but could not get working
+# have to park size for moment in order to make progress
+    # def __init__(self, *args, **kwargs):
+    #     super(Product, self).__init__(*args, **kwargs)
+
+    #     # Dynamically set size choices based on category
+    #     category = Category.objects.get(name="self.category")
+    #     if category.sizeable:
+    #         if category.name == 'Rings':
+    #             self.SIZE_CHOICES = self.RING_SIZES
+    #         elif category.name == 'Pendants':
+    #             self.SIZE_CHOICES = self.CHAIN_LENGTH
+    #         else:
+    #             #  Defensive warning needed, Should not get to this else section
+    #             self.SIZE_CHOICES = []
+    #     else:
+    #         self.SIZE_CHOICES = []
+
 
     def __str__(self):
         return self.name
