0d3a9f7e6b34f448e5de02a33a426e32d1dc5479
siobhain <sobrien@live.ie>
2023-10-30 01:28:32 +0000

    Checkout - Add views, templates & Crispy forms

--- memorylane_proj/settings.py
+++ memorylane_proj/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
     'storages',
     'bag',
     'checkout',
+    'crispy_forms',
 ]
 
 MIDDLEWARE = [
@@ -60,6 +61,8 @@ MIDDLEWARE = [
 ROOT_URLCONF = 'memorylane_proj.urls'
 MESSAGE_STORAGE = 'django.contrib.messages.storage.session.SessionStorage'
 
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
+
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
@@ -74,8 +77,13 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'django.template.context_processors.media',
                 'bag.contexts.bag_contents',
             ],
+            'builtins': [
+                'crispy_forms.templatetags.crispy_forms_tags',
+                'crispy_forms.templatetags.crispy_forms_field',
+            ]
         },
     },
 ]
