35ecad030af161624d8539a528c20bc3bae76bcc
siobhain <sobrien@live.ie>
2023-11-03 13:29:14 +0000

    Bag - Fix bug where quantity not displayed on number box & remove arrows from numberbox

--- bag/templatetags/bag_tools.py
+++ bag/templatetags/bag_tools.py
@@ -13,3 +13,13 @@ def calc_subtotal(price, quantity, size_this_row='none'):
         qty_this_row = quantity
 
     return price * qty_this_row
+
+@register.simple_tag
+def retrieve_quantity(quantity, size_this_row='none'):
+    if isinstance(quantity, dict):
+        inner_dict = quantity['items_by_size']
+        qty_this_row = inner_dict[size_this_row]
+    else:
+         qty_this_row = quantity
+
+    return qty_this_row    
